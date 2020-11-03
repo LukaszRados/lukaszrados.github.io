183fe894cfc9a446d3ed0830390624420a9d3531
Lukasz Rados <lukaszrados@gmail.com>
2020-11-03 18:19:05 +0100

    Posts categories.

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,9 +1,18 @@
 <template>
     <div class="ml-16 mr-4 max-w-4xl md:mx-auto lg:px-8 lg:mt-16 lg:mb-32">
-        <div class="mb-16">Filtry</div>
+        <div class="mb-16">
+            <button
+                type="button"
+                class="mr-4 hover:underline focus:outline-none"
+                v-for="category in categories"
+                :key="category.slug"
+                :class="{ 'underline': currentCategory === category.slug}"
+                @click="currentCategory = category.slug"
+            >#{{ category.title }}</button>
+        </div>
         <div class="md:grid md:grid-cols-2 md:gap-32">
             <div
-                v-for="post in posts"
+                v-for="post in postsFiltered"
                 :key="post.slug"
                 class="mb-24 md:mb-0"
             >
@@ -15,7 +24,13 @@
                     </h2>
                     <div class="flex items-center my-5">
                         <span class="w-1/2 lg:w-1/3">{{ post.published_at }}</span>
-                        <span class="w-1/2 lg:w-2/3">#{{ post.category }}</span>
+                        <span class="w-1/2 lg:w-2/3">
+                            <button
+                                class="hover:underline focus:outline-none"
+                                type="button"
+                                @click="currentCategory = post.category"
+                            >#{{ post.category }}</button>
+                        </span>
                     </div>
                     <p>{{ post.intro }}</p>
                 </fade-in-section>
@@ -27,10 +42,31 @@
 <script>
 export default {
     async asyncData ({ $content }) {
-        const posts = await $content('posts').fetch()
-        console.log(posts)
+        const posts = await $content('posts').sortBy('published_at', 'desc').fetch()
         return {
             posts
+        }
+    },
+
+    data () {
+        return {
+            currentCategory: '',
+            categories: [{
+                slug: '',
+                title: 'all'
+            }, {
+                slug: 'photography',
+                title: 'photography'
+            }, {
+                slug: 'coding',
+                title: 'coding'
+            }]
+        }
+    },
+
+    computed: {
+        postsFiltered () {
+            return this.currentCategory ? this.posts.filter(post => post.category === this.currentCategory) : this.posts
         }
     }
 }
