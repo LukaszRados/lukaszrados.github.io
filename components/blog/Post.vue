f5ee4ee3d48ade85af650cf26c46b789e54cbd9b
lukasz.rados <lukasz.rados@glovoapp.com>
2023-11-15 23:24:17 +0100

    feat: Post content styles

--- components/blog/Post.vue
+++ components/blog/Post.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>
-    <pre>{{ post }}</pre>
-    <h1>{{ post.title }}</h1>
-    <ContentRenderer :value="post" />
-  </div>
+  <article>
+    <section class="intro">
+      <NuxtLink to="/blog" class="back">
+        <UiArrow direction="left" />
+        Go back
+      </NuxtLink>
+      <h1 class="title">{{ post.title }}</h1>
+      <p class="excerpt">{{ post.intro }}</p>
+    </section>
+
+    <section class="content">
+      <ContentRenderer :value="post" />
+    </section>
+  </article>
 </template>
 
 <script setup lang="ts">
@@ -15,3 +24,133 @@ type Props = {
 
 defineProps<Props>();
 </script>
+
+<style lang="postcss">
+.back {
+  font-size: 0;
+}
+
+.intro {
+  margin: 0 auto 32px;
+  max-width: 900px;
+  padding: 0 16px;
+
+  .title {
+    font-size: 1.5rem;
+    font-weight: normal;
+    line-height: 1.375;
+    margin: 16px 0;
+  }
+
+  .excerpt {
+    font-size: 1.25rem;
+    line-height: 1.375;
+    margin: 0;
+  }
+}
+
+@media (min-width: 900px) {
+  .intro {
+    margin: 32px auto;
+
+    .title {
+      font-size: 3rem;
+      margin: 24px 0;
+    }
+
+    .excerpt {
+      font-size: 1.875rem;
+    }
+  }
+}
+
+@media screen and (min-width: 1200px) {
+  .intro {
+    margin: 64px auto;
+  }
+}
+
+.content {
+  line-height: 1.5;
+
+  h2,
+  h3,
+  p,
+  ul {
+    max-width: 900px;
+    margin: 0 auto;
+    padding: 0 16px;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    font-weight: normal;
+    margin-top: 32px;
+    margin-bottom: 16px;
+  }
+
+  h3 {
+    font-size: 1.5rem;
+    font-weight: normal;
+    margin-top: 24px;
+    margin-bottom: 16px;
+  }
+
+  p {
+    font-size: 1.125rem;
+    margin: 16px 0;
+  }
+
+  ul {
+    list-style: disc;
+    margin: 16px auto;
+    padding: 0;
+  }
+
+  li {
+    font-size: 1.25rem;
+    margin: 8px 0;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: underline;
+  }
+
+  a:hover {
+    text-decoration: none;
+  }
+
+  h2 a {
+    text-decoration: none;
+  }
+}
+
+@media screen and (min-width: 900px) {
+  .content {
+    h2,
+    h3,
+    p,
+    ul {
+      padding: 0 32px;
+    }
+
+    p {
+      font-size: 1.25rem;
+      margin: 32px auto;
+    }
+
+    h2 {
+      margin-top: 48px;
+      margin-bottom: 24px;
+      font-size: 1.875rem;
+    }
+
+    h3 {
+      margin-top: 32px;
+      margin-bottom: 20px;
+      font-size: 1.5rem;
+    }
+  }
+}
+</style>
