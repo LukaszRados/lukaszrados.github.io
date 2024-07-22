0bc0b86bc0f67b5b10f96077efa0f5838dfceaef
Lukasz Rados <lukaszrados@gmail.com>
2024-07-22 20:17:26 +0200

    Update en.js

--- lang/en.js
+++ lang/en.js
@@ -28,7 +28,7 @@ export default {
     description: "Personal website of Landscape Photographer & Frontend Developer",
     text_1: "I'm Łukasz Rados, I live in Barcelona and I'm a Frontend Developer at Preply. In the past I worked for Glovo, Booking.com, Woblink and Brandmorning.",
     text_2: "A few years ago I was bit by the travel bug and now take any opportunity to explore new countries in search of beautiful places to photograph. Whenever possible I combine this with my other hobbies: cycling and hiking.",
-    text_3: "In September 2019 I bought a sailing yacht with a group of friends. In a year we sailed 5000 nautical miles from England to Greece while exploring the coast of Europe. We were documented our adventure on {start_tag}a YouTube channel{end_tag}.",
+    text_3: "In September 2019 I bought a sailing yacht with a group of friends. In a year we sailed 5000 nautical miles from England to Greece while exploring the coast of Europe. We documented our adventure on {start_tag}a YouTube channel{end_tag}.",
   },
   lightbox: {
     close: "Close",
